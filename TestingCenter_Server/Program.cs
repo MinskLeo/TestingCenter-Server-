220dba415d8650147e9aa80655869e7131e7c5a1
Andrei <2119930@gmail.com>
2017-09-16 16:02:53 +0300

    Work_1

--- TestingCenter_Server/Program.cs
+++ TestingCenter_Server/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +11,47 @@ namespace TestingCenter_Server
 {
     class Program
     {
+        private static string IP_adr;
+        private static int Port;
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("IP адрес для прослушивания: (напр. 127.0.0.1)");
+                IP_adr = Console.ReadLine();
+                Console.WriteLine("Порт для прослушивания:");
+                Port = Convert.ToInt32(Console.ReadLine());
+                //Создаем листенер
+                IPAddress ip = IPAddress.Parse(IP_adr);
+                TcpListener tcp = new TcpListener(ip, Port);
+                tcp.Start();
+
+                ConsoleUpdatingInformation(ref tcp);//Отображение инфы  листенере
+                //Тут цикл листенера
+
+                //
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine(e.Message + "\n" + e.StackTrace);
+                Main(null);
+            }
+        }
+        private static void ConsoleUpdatingInformation(ref TcpListener tcp)
+        {
+            Console.Clear();
+            Console.WriteLine("IP: "+IP_adr);
+            Console.WriteLine("Port: "+Port);
+            Console.WriteLine("IncomingRequests: "+tcp.Pending());
+            if(tcp.Pending())
+            {
+                Console.WriteLine("Отвечаем на запрос...");
+            }
+            else
+            {
+                Console.WriteLine("Сервер запущен. Ожидание запросов...");
+            }
         }
     }
 }
+//Нужен метод для проверки корректоности ввода порта и айпишника. Иначе Демедович развалит кабину
